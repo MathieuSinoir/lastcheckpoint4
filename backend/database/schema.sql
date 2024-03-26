1a8274e64158ffe855d8355a56727d452943e623
Mathieu Sinoir <99977739+MathieuSinoir@users.noreply.github.com>
2024-03-26 20:22:49 +0100

    route put

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -17,7 +17,7 @@ username VARCHAR(255) NOT NULL
 CREATE TABLE team(
 id INT AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(250) NOT NULL,
-post VARCHAR(250) NOT NULL,
+poste VARCHAR(250) NOT NULL,
 description TEXT NOT NULL,
 creation_datetime DATETIME NOT NULL,
 user_id INT,
@@ -40,15 +40,15 @@ INSERT INTO user (email, password, username) VALUES
 
 
 
-INSERT INTO team(name, post, description, creation_datetime, user_id) VALUES
+INSERT INTO team(name, poste, description, creation_datetime, user_id) VALUES
 ('Ted Lasso', 'Coach', 'Coach de l équipe de Richmond', NOW(), 1),
-('Coach Beard', 'Coach', 'Coach en second de l équipe de Richmond', NOW(), 1),
-('Zoro', 'Gardien', 'Gardien de l équipe A', NOW(), 2),
-('McAdoo', 'Defenseur', 'Capitaine est defenseur rugeux', NOW(), 3),
-('Kent', 'Milieu', 'Joyaux de la couronne', NOW(), 4),
-('Obisanya', 'Milieu', 'Jeune Milieu fougeux', NOW(), 4),
-('Rojas', 'Attanquant', 'Fotball is life', NOW(), 5),
-('Tartt', 'Attaquant', 'Playboy du football', NOW(), 5);
+('Coach Beard', 'Coach', 'Coach en second de l équipe de Richmond', NOW(), 2),
+('Zoro', 'Gardien', 'Gardien de l équipe A', NOW(), 3),
+('McAdoo', 'Defenseur', 'Capitaine est defenseur rugeux', NOW(), 4),
+('Kent', 'Milieu', 'Joyaux de la couronne', NOW(), 5),
+('Obisanya', 'Milieu', 'Jeune Milieu fougeux', NOW(), 6),
+('Rojas', 'Attaquant', 'Fotball is life', NOW(), 7),
+('Tartt', 'Attaquant', 'Playboy du football', NOW(), 8);
 
 
 
--- backend/src/controllers/teamControllers.js
+++ backend/src/controllers/teamControllers.js
@@ -12,9 +12,9 @@ const getAllTeam = (req, res) => {
 };
 
 const getPlayersByPosition = (req, res) => {
-  const PlayerPositionId = req.params.id;
+  const PlayerPositionId = req.params.poste;
   database
-    .query("SELECT * FROM team WHERE user_id=?", [PlayerPositionId])
+    .query("SELECT * FROM team WHERE poste=?", [PlayerPositionId])
     .then(([players]) => {
       res.json(players);
     })
@@ -23,4 +23,42 @@ const getPlayersByPosition = (req, res) => {
     });
 };
 
-module.exports = { getAllTeam, getPlayersByPosition };
+const addPlayer = async (req, res) => {
+  const { name, poste, description, userId } = req.body;
+
+  try {
+    const results = await database.query(
+      "INSERT INTO team (name, poste, description, creation_datetime, userId) VALUES(?,?,?,NOW(),?)",
+      [name, poste, description, userId]
+    );
+
+    console.info(results);
+    res.status(200).send("Ajouté avec succès");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Une erreur est survenue lors de l'ajout du joueur");
+  }
+};
+
+const updatePlayer = (req, res) => {
+  const { id } = req.params; // Utilisation de la destructuration pour une meilleure clarté
+  const { name, poste, description } = req.body; // Idem ici pour les propriétés de req.body
+
+  database
+    .query(
+      "UPDATE team SET name = ?, poste = ?, description = ?, creation_datetime = NOW() WHERE id = ?",
+      [name, poste, description, id]
+    )
+    .then((result) => {
+      console.info(result);
+      res.send("Joueur modifié avec succès.");
+    })
+    .catch((err) => {
+      console.error(err);
+      res
+        .status(500)
+        .send("Une erreur s'est produite lors de la modification du joueur.");
+    });
+};
+
+module.exports = { getAllTeam, getPlayersByPosition, addPlayer, updatePlayer };
